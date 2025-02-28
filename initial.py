3bca0de93e527bda4d9166b896c8c1194fbe71de
saantigh <santi.gonzalezhincapie@gmail.com>
2025-02-28 18:58:42 -0500

    Se agrega la ventana principal

--- initial.py
+++ initial.py
@@ -1,17 +1,11 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from DesignWindows.ventana_principal import Ui_MainWindow  # Asegúrate de que el nombre coincida con tu archivo generado
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from DesignWindows.ventana_principal import Ui_MainWindow
 
-
-
-class MyApp(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-# Inicializar la aplicación
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyApp()
-    window.show()
+    MainWindow = QMainWindow()        # Crea la ventana principal
+    ui = Ui_MainWindow()              # Instancia la interfaz generada
+    ui.setupUi(MainWindow)            # Configura la UI en la ventana principal
+    MainWindow.show()
     sys.exit(app.exec())
